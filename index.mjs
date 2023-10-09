93ebe4b79d0cba5d0f8b00dbe693558312580809
Pedro Ramalho <pedro.ramalho@sysmap.com.br>
2023-10-08 21:24:36 -0300

    implementacao lambda

---
+++ index.mjs
@@ -0,0 +1,9 @@
+export const handler = async (event) => {
+    // TODO implement
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify('Appplication is working!')
+    };
+    return response;
+  };
+  
