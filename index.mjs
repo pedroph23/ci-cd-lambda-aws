3947be7edcda18b83e52706eaee27b22af40cee6
Pedro Ramalho <pedro.ramalho@sysmap.com.br>
2023-10-08 21:25:39 -0300

    ajuste

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
     // TODO implement
     const response = {
       statusCode: 200,
-      body: JSON.stringify('Appplication is working!')
+      body: JSON.stringify('Application is working!')
     };
     return response;
   };
